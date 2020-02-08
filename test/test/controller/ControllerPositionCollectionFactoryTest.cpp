7562ba0f021b44bff7fb6c9d89c9aeae7408552a
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-02-08 16:28:11 +0000

    Fix #92: Controller Positions With Zero Frequencies

--- test/test/controller/ControllerPositionCollectionFactoryTest.cpp
+++ test/test/controller/ControllerPositionCollectionFactoryTest.cpp
@@ -39,7 +39,17 @@ namespace UKControllerPluginTest {
             EXPECT_EQ(0, collection->GetSize());
         }
 
-        TEST(ControllerPositionCollectionFactory, AddsControllerIfValid)
+        TEST_F(ControllerPositionCollectionFactoryTest, AddsDoesntAddControllerIfNotValid)
+        {
+            ON_CALL(this->dependency, LoadDependency("DEPENDENCY_CONTROLLER_POSITIONS", nlohmann::json::object()))
+                .WillByDefault(Return("{\"EGAA_GND\": {\"frequency\": \"test\", \"top-down\" : [\"EGAA\"]}}"_json));
+            std::shared_ptr<ControllerPositionCollection> collection = ControllerPositionCollectionFactory::Create(
+                dependency
+            );
+            EXPECT_EQ(0, collection->GetSize());
+        }
+
+        TEST_F(ControllerPositionCollectionFactoryTest, AddsControllerIfValid)
         {
             DependencyCache dependency;
             dependency.AddDependency(
@@ -52,6 +62,20 @@ namespace UKControllerPluginTest {
             ControllerPosition position = collection->FetchPositionByCallsign("EGAA_GND");
             EXPECT_EQ(0, position.GetCallsign().compare("EGAA_GND"));
             EXPECT_EQ(121.750, position.GetFrequency());
+            EXPECT_EQ(0, position.GetType().compare("GND"));
+            EXPECT_THAT(position.GetTopdown(), ElementsAre("EGAA"));
+        }
+
+        TEST_F(ControllerPositionCollectionFactoryTest, AddsControllerIfValidWithZeroEndingFrequency)
+        {
+            ON_CALL(this->dependency, LoadDependency("DEPENDENCY_CONTROLLER_POSITIONS", nlohmann::json::object()))
+                .WillByDefault(Return("{\"EGAA_GND\": {\"frequency\": 121.000, \"top-down\" : [\"EGAA\"]}}"_json));
+            std::shared_ptr<ControllerPositionCollection> collection = ControllerPositionCollectionFactory::Create(
+                dependency
+            );
+            ControllerPosition position = collection->FetchPositionByCallsign("EGAA_GND");
+            EXPECT_EQ(0, position.GetCallsign().compare("EGAA_GND"));
+            EXPECT_EQ(121.000, position.GetFrequency());
             EXPECT_EQ(0, position.GetType().compare("GND"));
             EXPECT_THAT(position.GetTopdown(), ElementsAre("EGAA"));
         }
